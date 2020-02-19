d7ecc6860e2948db7935e6948af59a13f1915e4a
Courtney Goodridge [RPG] <pscmgo@leeds.ac.uk>
2020-02-19 10:03:42 +0000

    submitted CV

--- make_pdf.R
+++ make_pdf.R
@@ -1,2 +1,2 @@
 setwd("C:/Users/pscmgo/OneDrive for Business/PhD/Project/Experiment_Code/resume")
-pagedown::chrome_print("index.html", "goodridge.pdf")
+pagedown::chrome_print("index.html", "goodridge_CV.pdf")
